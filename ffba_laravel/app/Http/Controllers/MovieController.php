86438b2c94f6e7b4198ab7df5ac0ecc977af92c5
cihanrm <118432836+cihanrm@users.noreply.github.com>
2023-10-04 18:42:45 +0200

    Finish

--- ffba_laravel/app/Http/Controllers/MovieController.php
+++ ffba_laravel/app/Http/Controllers/MovieController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use GuzzleHttp\Client; // Importez la classe Client depuis Guzzle
+use GuzzleHttp\Client;
 
 class MovieController extends Controller
 {
